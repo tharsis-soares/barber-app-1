6f6e2b23f52bc0cb439318407afd27e92a0ea448
tharsis-soares <50808840+tharsis-soares@users.noreply.github.com>
2021-04-15 22:44:07 -0300

    React Native App

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
 
 export default function App() {
   return (
